a20915e19eee9287ffef500cf01828bf21076d07
Angelo Evangelista <angeloevan.ane@gmail.com>
2021-02-21 22:20:31 -0300

    Usando unsubscribe

--- desenv-avancado-angular/02-promises-e-observables/RXJS/src/app/app.component.ts
+++ desenv-avancado-angular/02-promises-e-observables/RXJS/src/app/app.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit } from "@angular/core";
 import { Observable, Observer } from "rxjs";
 
+class Usuario {
+  nome: string;
+  email: string;
+
+  constructor(nome: string, email: string) {
+    this.nome = nome;
+    this.email = email;
+  }
+}
+
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
@@ -38,6 +48,39 @@ export class AppComponent implements OnInit {
     });
   }
 
+  usuarioObservable(nome: string, email: string): Observable<Usuario> {
+    return new Observable((subscriber) => {
+      if (!!nome.length) {
+        const usuario = new Usuario(nome, email);
+
+        setTimeout(() => {
+          subscriber.next(usuario);
+        }, 1000);
+
+        setTimeout(() => {
+          subscriber.next(usuario);
+        }, 2000);
+
+        setTimeout(() => {
+          subscriber.next(usuario);
+        }, 3000);
+
+        setTimeout(() => {
+          subscriber.next(usuario);
+        }, 4000);
+
+        setTimeout(() => {
+          subscriber.next(usuario);
+          subscriber.complete();
+        }, 5000);
+
+        return;
+      }
+
+      subscriber.error("Opa, preciso saber o seu nome, jovem :/");
+    });
+  }
+
   ngOnInit(): void {
     // this.minhaPromise("Angelão")
     //   .then((response) => console.log(response))
@@ -48,14 +91,28 @@ export class AppComponent implements OnInit {
     //   (error) => console.error(error)
     // );
 
-    const observer: Observer<string> = {
+    // const observer: Observer<string> = {
+    //   next: (value) => console.log("Next:", value),
+    //   error: (err) => console.error("Error:", err),
+    //   complete: () => console.log("Complete"),
+    // };
+
+    // const observable = this.minhaObservable("Angelo");
+    // observable.subscribe(observer);
+
+    const usuarioObserver: Observer<Usuario> = {
       next: (value) => console.log("Next:", value),
       error: (err) => console.error("Error:", err),
       complete: () => console.log("Complete"),
     };
 
-    const observable = this.minhaObservable("Angelo");
+    const observable = this.usuarioObservable(
+      "Angelo",
+      "angeloevan.ane@gmail.com"
+    );
 
-    observable.subscribe(observer);
+    const subscription = observable.subscribe(usuarioObserver);
+
+    setTimeout(() => subscription.unsubscribe(), 3000);
   }
 }
